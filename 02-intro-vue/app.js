70ab86a58dfe28c5fc2cfd4a80abd3bbdbe4cf4c
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-03-25 10:07:12 -0300

    variables reactivas

--- 02-intro-vue/app.js
+++ 02-intro-vue/app.js
@@ -1,11 +1,21 @@
 const { createApp, ref } = Vue;
 
 const app = createApp({
- template: `
+  template: `
    <div>
-     <h1>Hola Mundo en Vue desde app.js</h1>
+     <h1>{{message}}</h1>
+     <p>Written by {{author}}</p>
    </div>
- `
+ `,
+  setup() {
+    const message = ref("I'm Batman");
+    const author = ref("Bruce Wayne");
+    setTimeout(() => {
+      message.value = "Soy Goku";
+      author.value = "Kakaroto";
+    }, 1000);
+    return { message, author };
+  },
 });
 
-app.mount('#app');
+app.mount("#app");
