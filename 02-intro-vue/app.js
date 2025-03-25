0d067590fde761b1e3713328d33809823ef26b2e
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-03-25 10:16:15 -0300

    separar html y eventos

--- 02-intro-vue/app.js
+++ 02-intro-vue/app.js
@@ -1,20 +1,22 @@
 const { createApp, ref } = Vue;
 
 const app = createApp({
-  template: `
-   <div>
-     <h1>{{message}}</h1>
-     <p>Written by {{author}}</p>
-   </div>
- `,
+//   template: `
+//    <div>
+//      <h1>{{message}}</h1>
+//      <p>Written by {{author}}</p>
+//    </div>
+//  `,
   setup() {
     const message = ref("I'm Batman");
     const author = ref("Bruce Wayne");
-    setTimeout(() => {
-      message.value = "Soy Goku";
-      author.value = "Kakaroto";
-    }, 1000);
-    return { message, author };
+   
+    const changeMessage = () => {
+      message.value = "I'm Ironman";
+      author.value = "Tony Stark";
+    }
+
+    return { message, author, changeMessage };
   },
 });
 
