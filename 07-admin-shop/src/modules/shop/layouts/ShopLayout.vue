eeb981b7006f5399651fbbb5e894d58d5e1cfece
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-04-07 12:02:33 -0400

    inicio de proyecto y estructura de layout shop mas el backend

---
+++ 07-admin-shop/src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <TopMenu />
+  <HomeView />
+  <CustomFooter />
+</template>
+
+<script setup lang="ts">
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
+import HomeView from '../views/HomeView.vue';
+
+</script>
---
+++ 07-admin-shop/src/router/index.ts
@@ -0,0 +1,23 @@
+import ShopLayout from '@/modules/shop/layouts/ShopLayout.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'shop',
+      component: ShopLayout,
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: () => import('@/modules/shop/views/HomeView.vue')
+        }
+      ]
+    },
+  ],
+})
+
+export default router
